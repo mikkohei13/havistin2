615fc8c9c700f9cc5d217e170a82392544fffae3
Mikko Heikkinen <hawk@biomi.org>
2022-08-02 23:52:47 +0300

    List of species with some basic information

---
+++ app/taxa/common.py
@@ -0,0 +1,49 @@
+import requests
+import json
+import sys
+import re
+
+import app_secrets
+
+#TODO: combine generic functions with atlas/common.py
+
+def print_log(dict):
+    print(dict, sep="\n", file = sys.stdout)
+
+
+def valid_qname(qname):
+    pattern = r'[A-Z]+\.[A-Z0-9]+'
+    match = re.fullmatch(pattern, qname)
+
+    if match is not None:
+        return qname
+    else:
+        print_log("ERROR: Qname invalid: " + qname)
+        raise ValueError
+
+
+def fetch_finbif_api(api_url, log = False):
+    api_url = api_url + app_secrets.finbif_api_token
+#    print(api_url, file = sys.stdout)
+
+    if log:
+        print_log(api_url)
+
+    try:
+        r = requests.get(api_url)
+    except ConnectionError:
+        print("ERROR: api.laji.fi complete error.", file = sys.stdout)
+
+#    r.encoding = encoding
+    dataJson = r.text
+    dataDict = json.loads(dataJson)
+
+    if "status" in dataDict:
+        if 403 == dataDict["status"]:
+            print("ERROR: api.laji.fi 403 error.", file = sys.stdout)
+            raise ConnectionError
+
+#    print(dataDict, file = sys.stdout)
+    return dataDict
+
+
--- app/taxa/specieslist.py
+++ app/taxa/specieslist.py
@@ -1,7 +1,44 @@
 
+import taxa.common as common
+
+def generate_list(data):
+
+    family_mem = ""
+    html = ""
+
+    for item in data["results"]:
+        family = item["parent"]["family"]["scientificName"]
+        if family != family_mem:
+            html += "</ul>"
+            html += "<h4>" + family + "</h4>"
+            html += "<ul>"
+            family_mem = family
+
+        html += "<li>"
+        html += f"<a href='/taxa/species/{item['id']}'>"
+        html += item["scientificName"] + " - " + item["vernacularName"]
+        html += "</a>"
+        html += f" ({item['observationCountFinland']}) {item['typeOfOccurrenceInFinland'][0]}"
+        html += "</li>"
+
+        common.print_log(item["vernacularName"])
+
+    html += "</ul>"
+    return html
 
 def main(taxon_id_untrusted):
+
+    valid_qname = common.valid_qname(taxon_id_untrusted)
+    
     html = dict()
-    html["taxon_name"] = "Luteet"
+    html["taxon_name"] = valid_qname
+
+    data = common.fetch_finbif_api("https://api.laji.fi/v0/taxa/MX.229577/species?onlyFinnish=true&selectedFields=id,vernacularName,scientificName,typeOfOccurrenceInFinland,parent.family.scientificName,observationCountFinland&lang=fi&page=1&pageSize=10&sortOrder=taxonomic&access_token=")
+
+    common.print_log(data)
+
+    html["list"] = generate_list(data)
+
+# https://api.laji.fi/v0/taxa/MX.229577/species?onlyFinnish=true&selectedFields=id,vernacularName,scientificName,typeOfOccurrenceInFinland,primaryHabitat,secondaryHabitats,latestRedListStatusFinland&lang=fi&page=1&pageSize=1000&sortOrder=taxonomic&access_token=8N7b0asX4eKbbv8VaLOFLRI9wev5iGYuTekXBmsohSRKy5bvcfqHFYBGo1ohymSm
 
     return html
