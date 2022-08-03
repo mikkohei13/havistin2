d6cd3218f4dfe09766698cf2e4ce5fda4c846fa2
Mikko Heikkinen <hawk@biomi.org>
2022-08-03 09:40:03 +0300

    More informaton about a taxon

--- app/taxa/specieslist.py
+++ app/taxa/specieslist.py
@@ -31,13 +31,20 @@ def main(taxon_id_untrusted):
     valid_qname = common.valid_qname(taxon_id_untrusted)
     
     html = dict()
-    html["taxon_name"] = valid_qname
 
-    data = common.fetch_finbif_api("https://api.laji.fi/v0/taxa/MX.229577/species?onlyFinnish=true&selectedFields=id,vernacularName,scientificName,typeOfOccurrenceInFinland,parent.family.scientificName,observationCountFinland&lang=fi&page=1&pageSize=10&sortOrder=taxonomic&access_token=")
+    taxon_data = common.fetch_finbif_api(f"https://api.laji.fi/v0/taxa/{valid_qname}?lang=fi&langFallback=true&maxLevel=0&includeHidden=false&includeMedia=false&includeDescriptions=false&includeRedListEvaluations=false&sortOrder=taxonomic&access_token=")
 
-    common.print_log(data)
+    html["vernacular_name"] = taxon_data["vernacularName"]
+    html["finnish_species_count"] = taxon_data["countOfFinnishSpecies"]
+    html["scientific_name"] = taxon_data["scientificName"]
+    html["obs_count_finland"] = taxon_data["observationCountFinland"]
 
-    html["list"] = generate_list(data)
+    max_species = 20
+    species_data = common.fetch_finbif_api(f"https://api.laji.fi/v0/taxa/{valid_qname}/species?onlyFinnish=true&selectedFields=id,vernacularName,scientificName,typeOfOccurrenceInFinland,parent.family.scientificName,observationCountFinland&lang=fi&page=1&pageSize={max_species}&sortOrder=taxonomic&access_token=")
+
+#    common.print_log(taxon_data)
+
+    html["list"] = generate_list(species_data)
 
 # https://api.laji.fi/v0/taxa/MX.229577/species?onlyFinnish=true&selectedFields=id,vernacularName,scientificName,typeOfOccurrenceInFinland,primaryHabitat,secondaryHabitats,latestRedListStatusFinland&lang=fi&page=1&pageSize=1000&sortOrder=taxonomic&access_token=8N7b0asX4eKbbv8VaLOFLRI9wev5iGYuTekXBmsohSRKy5bvcfqHFYBGo1ohymSm
 
