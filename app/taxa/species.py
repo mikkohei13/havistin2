a29c6808a4df043ef580404f79af16d89fc72d67
Mikko Heikkinen <hawk@biomi.org>
2022-08-03 11:25:17 +0300

    Prepare for making fact aggregates

--- app/taxa/species.py
+++ app/taxa/species.py
@@ -1,15 +1,29 @@
 
 import taxa.common as common
 
+def get_obs_aggregate_data(qname):
+    limit = 100
+
+    # counts etc.
+    # https://api.laji.fi/v0/warehouse/query/unit/list?selected=gathering.notes%2Cunit.abundanceString%2Cunit.abundanceUnit%2Cunit.identifications.notes%2Cunit.notes&pageSize=100&page=1&cache=false&taxonId=MX.230528&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&countryId=ML.206&individualCountMin=1&qualityIssues=NO_ISSUES&access_token=
+
+    # facts
+    # https://api.laji.fi/v0/warehouse/query/unit/list?selected=gathering.facts.decimalValue%2Cgathering.facts.fact%2Cgathering.facts.integerValue%2Cgathering.facts.value%2Cunit.facts.decimalValue%2Cunit.facts.fact%2Cunit.facts.integerValue%2Cunit.facts.value&pageSize=100&page=1&cache=false&taxonId=MX.230528&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&countryId=ML.206&individualCountMin=1&qualityIssues=NO_ISSUES&access_token=
+
+    species_data = common.fetch_finbif_api(f"https://api.laji.fi/v0/warehouse/query/unit/list?pageSize={limit}&page=1&cache=true&taxonId={qname}&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&countryId=ML.206&individualCountMin=1&qualityIssues=NO_ISSUES&access_token=")
+
+    return species_data
+
 
 def main(taxon_id_untrusted):
 
-    valid_qname = common.valid_qname(taxon_id_untrusted)
+    qname = common.valid_qname(taxon_id_untrusted)
     
     html = dict()
 
+    # Get basic taxon data
     # &selectedFields=id,vernacularName,scientificName,typeOfOccurrenceInFinland,parent.family.scientificName,observationCountFinland
-    species_data = common.fetch_finbif_api(f"https://api.laji.fi/v0/taxa/{valid_qname}/species?onlyFinnish=true&lang=fi&page=1&pageSize=10&sortOrder=taxonomic&access_token=")
+    species_data = common.fetch_finbif_api(f"https://api.laji.fi/v0/taxa/{qname}/species?onlyFinnish=true&lang=fi&page=1&pageSize=10&sortOrder=taxonomic&access_token=")
 
     # Expecting only one species
     html["raw_data"] = species_data["results"][0]
@@ -23,15 +37,7 @@ def main(taxon_id_untrusted):
     html["redlist_status"] = common.fetch_variable_label(html["raw_data"]["latestRedListStatusFinland"]["status"])
     html["redlist_year"] = html["raw_data"]["latestRedListStatusFinland"]["year"]
 
-    '''
-    <p>Esiintymisen tyyppi: typeOfOccurrenceInFinland</p>
-
-    <p>Ensisijainen habitaatti: primaryHabitat:habitat</p>
-
-    <p>Id: qname</p>
-
-    <p>Uhanalaisuusluokka: latestRedListStatusFinland:status / year</p>
-    '''
+    html["raw_occurrences"] = get_obs_aggregate_data(qname)
 
 
 #    html["scientific_name"] = species_data["scientificName"]
