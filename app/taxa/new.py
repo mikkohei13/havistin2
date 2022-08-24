e981bdd3862a496589f2584031f935df01c5d493
Mikko Heikkinen <hawk@biomi.org>
2022-08-24 11:28:44 +0300

    Styles, image handling improvements

--- app/taxa/new.py
+++ app/taxa/new.py
@@ -9,6 +9,10 @@ def get_species_qnames(qname):
         
         species.append("MX.5075811")
         species.append("MX.229664")
+        #species.append("MX.230068") # debug duplicate
+        #species.append("MX.4994296") # debug duplicate
+        #species.append("MX.230504") # debug duplicate
+        
         species.append("MX.4999057")
         species.append("MX.5080747")
         species.append("MX.5074723")
@@ -56,7 +60,7 @@ def get_species_qnames(qname):
         species.append("MX.5015646")
         species.append("MX.230548")
         species.append("MX.230556")
-
+        
     return species
 
 def get_species_data(species_qnames):
@@ -69,61 +73,108 @@ def get_species_data(species_qnames):
     return species_data
 
 
+def get_additional_multimedia(qname):
+    multimedia = [] # list of dicts
+
+    # Verified observations
+    data = common.fetch_finbif_api(f"https://api.laji.fi/v0/warehouse/query/unitMedia/list?taxonId={qname}&reliability=RELIABLE&aggregateBy=unit.linkings.taxon.id,media,document.documentId,unit.unitId&selected=unit.linkings.taxon.id,media,document.documentId,unit.unitId&includeNonValidTaxa=false&hasUnitMedia=true&cache=true&page=1&pageSize=4&access_token=")
+
+#    common.print_log("GETTING MORE IMAGES")
+#    common.print_log(data)
+
+    # If still no verified records, try specimens 
+    if 0 == data['total']:
+        data = common.fetch_finbif_api(f"https://api.laji.fi/v0/warehouse/query/unitMedia/list?taxonId={qname}&sourceId=KE.3&superRecordBasis=PRESERVED_SPECIMEN&aggregateBy=unit.linkings.taxon.id,media,document.documentId,unit.unitId&selected=unit.linkings.taxon.id,media,document.documentId,unit.unitId&includeNonValidTaxa=false&hasUnitMedia=true&cache=true&needsCheck=false&page=1&pageSize=4&access_token=")
+
+    # If still no photos, return empty 
+    if 0 == data['total']:
+        return multimedia, False
+
+    for item in data['results']:
+        media = dict()
+        if "IMAGE" == item['media']['mediaType']:
+            # TODO: harmonize cc-links to abbreviation
+            media['licenseAbbreviation'] = item['media']['licenseId']
+            media['fullURL'] = item['media']['fullURL']
+            media['thumbnailURL'] = item['media']['thumbnailURL']
+            media['author'] = item['media']['author']
+            media['caption'] = "Havainto/näyte " + item['document']['documentId']
+
+            multimedia.append(media)
+
+    return multimedia, True
+
+
 def generate_media_html(media_data, qname):
+
+    disallowed = ["ARR", "http://tun.fi/MZ.intellectualRightsARR"]
+
     html = ""
     for media in media_data:
-        html += "<figure class='photo'>"
-        if media['licenseAbbreviation'] != "arr":
+        if media['licenseAbbreviation'] not in disallowed:
+            html += "\n<figure class='photo'>\n"
             html += "<a href='" + media['fullURL'] + "'><img src='" + media['thumbnailURL'] + "' alt='' title=''></a>"
-            html += "<figcaption>"
+            html += "<figcaption>\n"
             if "caption" in media:
-                html += "<span class='caption'>" + media['caption'] + "</span><br>"
+                html += "<span class='caption'>" + media['caption'] + "</span><br>\n"
             html += "<span class='copyright'>" + media['author'] + ", " + media['licenseAbbreviation'] + "</span>"
-            html += "</figcaption>"
+            html += "</figcaption>\n"
+            html += "</figure>\n"
         else:
-            html += f"Ei Creative Commons -lisensoituja kuvia, <a href='https://laji.fi/taxon/{qname}/images'>katso kuvat Laji.fi:n lajisivulta</a>."
-        html += "</figure>"
+            html += "<!-- non-cc media -->\n"
 
     return html
 
 def generate_species_html(species_data):
-    html = "<!-- Here shall be species -->"
+    html = ""
 
     family_mem = ""
 
     for species in species_data:
+        qname = species['qname']
         family = species['parent']['family']['scientificName']
+
         if family != family_mem:
             html += f"<h2 class='new_family'>{family}</h2>"
             family_mem = family
 
         html += "\n<div class='species'>\n"
         if "vernacularName" in species:
-            html += f"<h3><em>{species['vernacularName']}</em> - {species['scientificName']} {species['scientificNameAuthorship']}</h3>\n"
+            vernacular_name = species['vernacularName'].capitalize()
+            html += f"<h3><em>{vernacular_name}</em> - {species['scientificName']} {species['scientificNameAuthorship']}</h3>\n"
         else:
             html += f"<h3><em>{species['scientificName']}</em> {species['scientificNameAuthorship']}</h3>\n"
 
         html += "<ul class='speciesinfo'>\n"
 
-        # TODO: Can there be multiple statuses?
+        # TODO: Can there be multiple statuses? Yes.
         if "typeOfOccurrenceInFinland" in species:
-            status = common.map_status(species['typeOfOccurrenceInFinland'][0])
-            html += f"<li>{status}"
+            html += "<li>"
+            for status in species['typeOfOccurrenceInFinland']:
+                translated_status = common.map_status(status)
+                html += f"{translated_status}, "
         else:
             html += "<li>ei statustietoa"
 
-        html += " - <a href='/taxa/species/" + species['qname'] + "'>lisätietoa &raquo;</a></li>\n"
+        html += " <a href='/taxa/species/" + qname + "'>lisätietoa &raquo;</a></li>\n"
         
-        html += "<li><a href='https://laji.fi/observation/map?target=" + species['qname'] + "&countryId=ML.206&recordQuality=EXPERT_VERIFIED,COMMUNITY_VERIFIED,NEUTRAL&needsCheck=false'>" + str(species['observationCountFinland']) + " havaintoa Suomesta</a></li>\n"
+        html += "<li>Laji.fi:ssa <a href='https://laji.fi/observation/map?target=" + qname + "&countryId=ML.206&recordQuality=EXPERT_VERIFIED,COMMUNITY_VERIFIED,NEUTRAL&needsCheck=false'>" + str(species['observationCountFinland']) + " havaintoa Suomesta</a>, <a href='https://laji.fi/taxon/" + qname + "'>lajisivu</a></li>\n"
 
         html += "</ul>\n"
 
-        html += "<div class='multimedia'>"
-        if "multimedia" in species:
+        # Photos
+        # If no taxon photos, try different sources
+        if "multimedia" not in species:
+            species['multimedia'], got_images = get_additional_multimedia(qname)
+            species['hasMultimedia'] = got_images
+
+        # Create photo html
+        html += "<div class='multimedia'>\n"
+        if True == species['hasMultimedia']:
             html += generate_media_html(species['multimedia'], species['qname'])
         else:
             html += "ei kuvia"
-        html += "</div>"
+        html += "</div>\n"
 
 #        html += json.dumps(species) # debug
         html += "</div>\n"
