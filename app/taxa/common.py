aabe468a02632155cc89ed7edfb20511963ca18d
Mikko Heikkinen <hawk@biomi.org>
2022-09-11 17:55:02 +0300

    Get photos frim iNat as well

--- app/taxa/common.py
+++ app/taxa/common.py
@@ -34,6 +34,21 @@ def valid_qname(qname):
         raise ValueError
 
 
+def fetch_api(api_url, log = False):
+    if log:
+        print_log(api_url)
+    try:
+        r = requests.get(api_url)
+    except ConnectionError:
+        print("ERROR: API connection error.", file = sys.stdout)
+    dataDict = json.loads(r.text)
+    if "status" in dataDict:
+        if 403 == dataDict["status"]:
+            print("ERROR: API 403 error.", file = sys.stdout)
+            raise ConnectionError
+    return dataDict
+
+
 def fetch_finbif_api(api_url, log = False):
     api_url = api_url + app_secrets.finbif_api_token
 #    print(api_url, file = sys.stdout)
@@ -44,7 +59,7 @@ def fetch_finbif_api(api_url, log = False):
     try:
         r = requests.get(api_url)
     except ConnectionError:
-        print("ERROR: api.laji.fi complete error.", file = sys.stdout)
+        print("ERROR: api.laji.fi connection error.", file = sys.stdout)
 
 #    r.encoding = encoding
     dataJson = r.text
