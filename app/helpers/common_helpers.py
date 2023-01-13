6e0117a2e9793e26c119aee6db7a9fb18b4ea766
Mikko Heikkinen <hawk@biomi.org>
2023-01-13 18:23:00 +0200

    Set up helpers, get number of today's observations

---
+++ app/helpers/common_helpers.py
@@ -0,0 +1,3 @@
+
+def print_foo():
+    print("FOOOOOOO!")
--- app/index/index.py
+++ app/index/index.py
@@ -5,9 +5,12 @@ import sys
 
 import app_secrets
 
+from helpers import common_helpers
+
 def check_api():
 
-    api_url = "https://apiX.laji.fi/v0/warehouse/query/unit/aggregate?aggregateBy=document.collectionId&onlyCount=true&taxonCounts=false&pairCounts=false&atlasCounts=false&excludeNulls=true&pessimisticDateRangeHandling=false&pageSize=1&page=1&cache=true&taxonId=MX.37580&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&countryId=ML.206&individualCountMin=1&access_token="
+    # Check by fetching 10 latest observations from today
+    api_url = "https://api.laji.fi/v0/warehouse/query/list?countryId=ML.206&time=0/0&aggregateBy=unit.interpretations.recordQuality,document.linkings.collectionQuality,unit.linkings.taxon.taxonomicOrder,unit,unit.abundanceString,gathering.displayDateTime,gathering.interpretations.countryDisplayname,gathering.interpretations.biogeographicalProvinceDisplayname,gathering.locality,document.collectionId,document.documentId,gathering.team,document.secureLevel,unit.unitId,document.documentId&selected=unit.interpretations.recordQuality,document.linkings.collectionQuality,unit.linkings.taxon.taxonomicOrder,unit,unit.abundanceString,gathering.displayDateTime,gathering.interpretations.countryDisplayname,gathering.interpretations.biogeographicalProvinceDisplayname,gathering.locality,document.collectionId,document.documentId,gathering.team,document.secureLevel,unit.unitId,document.documentId&cache=true&page=1&pageSize=10&access_token="
     api_url += app_secrets.finbif_api_token
     print(api_url)
 
@@ -17,18 +20,18 @@ def check_api():
     except requests.exceptions.HTTPError as e:
         print("Havistin error: HTTPError.", file = sys.stdout)
         return False
-#    except requests.exceptions.ConnectionError as e:
-#        print("Havistin error: ConnectionError.", file = sys.stdout)
-#        return False
+    except requests.exceptions.ConnectionError as e:
+        print("Havistin error: ConnectionError.", file = sys.stdout)
+        return False
     except requests.exceptions.Timeout as e:
         print("Havistin error: Timeout.", file = sys.stdout)
         return False
     except requests.exceptions.TooManyRedirects as e:
         print("Havistin error: TooManyRedirects.", file = sys.stdout)
         return False
-#    except requests.exceptions.RequestException as e:
-#        print("Havistin error: RequestException.", file = sys.stdout)
-#        return False
+    except requests.exceptions.RequestException as e:
+        print("Havistin error: RequestException.", file = sys.stdout)
+        return False
 
     dataJson = r.text
 
@@ -47,20 +50,27 @@ def check_api():
             print("Havistin error: api.laji.fi 404 error: " + str(dataDict["message"]), file = sys.stdout)
             return False
 
-    return True
+    # Todo: how these are logged? On Google Cloud docs?
+
+#    print(dataDict, file = sys.stdout)
+    return dataDict
+
 
 def main():
     html = dict()
 
+    common_helpers.print_foo()
+
     api_up = check_api()
 
-    if (api_up):
-        html["foo"] = "API is up"
-        # Give links, show stats
-    else:
-        html["foo"] = "API is DOWN"
-        # Show error, give links
+    # This is special for front page: check that API is up and give error for user if not.
+    html["error"] = ""
+    if api_up == False:
+        html["error"] = "<p id='apierror'>Lajitietokeskuksen rajapinta ei juuri nyt toimi. Ole hyvä ja yritä myöhemmin uudelleen!</p>"
+        return html
 
+    # API works, proceed normally
+    html["today_obs_count"] = api_up["total"]
 
 
     return html
