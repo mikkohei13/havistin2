12ac85eda5212f52cae845bb07d02ce18bd58df0
Mikko Heikkinen <hawk@biomi.org>
2023-01-14 23:06:26 +0200

    Add CC-links

--- app/helpers/common_helpers.py
+++ app/helpers/common_helpers.py
@@ -12,23 +12,37 @@ def print_log(dict):
     print(dict, sep="\n", file = sys.stdout)
 
 
+def cc_link_html(link, name):
+    return f"<a href='{link}'>{name}</a>"
+
+
 # TODO: make full list of possible licenses
 # TODO: make license link
 def cc_abbreviation(lic):
+
     if "http://tun.fi/MZ.intellectualRightsCC-BY-NC-4.0" == lic or "CC-BY-NC-4.0" == lic or "CC BY-NC 4.0" == lic:
-        return "CC BY-NC 4.0"
+        return cc_link_html("https://creativecommons.org/licenses/by-nc/4.0/", "CC BY-NC 4.0")
+
     if "http://tun.fi/MZ.intellectualRightsCC-BY-SA-4.0" == lic or "CC-BY-SA-4.0" == lic:
-        return "CC BY-SA 4.0"
+        return cc_link_html("https://creativecommons.org/licenses/by-sa/4.0/", "CC BY-SA 4.0")
+
     if "http://tun.fi/MZ.intellectualRightsCC-BY-4.0" == lic or "CC-BY-4.0" == lic:
-        return "CC BY 4.0"
+        return cc_link_html("https://creativecommons.org/licenses/by/4.0/", "CC BY 4.0")
+
     if "http://tun.fi/MZ.intellectualRightsCC-BY-NC-ND-4.0" == lic or "CC-BY-NC-ND-4.0" == lic:
-        return "CC BY-NC-ND 4.0"
+        return cc_link_html("https://creativecommons.org/licenses/by-nc-nd/4.0/", "CC BY-NC 4.0")
+
     if "http://tun.fi/MZ.intellectualRightsCC-BY-NC-SA-4.0" == lic or "CC-BY-NC-SA-4.0" == lic:
-        return "CC BY-NC-SA 4.0"
+        return cc_link_html("https://creativecommons.org/licenses/by-nc-sa/4.0/", "CC BY-NC 4.0")
+
     if "CC0-4.0" == lic:
-        return "CC Zero 4.0"
+        return cc_link_html("https://creativecommons.org/share-your-work/public-domain/cc0/", "CC Zero 4.0")
+
     if "pd" == lic:
         return "Public Domain"
+
+    if "http://tun.fi/MZ.intellectualRightsARR" == lic:
+        return "Kaikki oikeudet pidätetään"
 
 #    if "" == lic or "" == lic:
 #        return ""
@@ -36,8 +50,11 @@ def cc_abbreviation(lic):
 
 
 def fetch_finbif_api(api_url, log = False):
+    if "&access_token=" not in api_url:
+        print("DEV WARNING: access_token param is missing from your url!", file = sys.stdout)
+
     api_url = api_url + app_secrets.finbif_api_token
-#    print(api_url, file = sys.stdout)
+    print("Fetching API: " + api_url, file = sys.stdout)
 
     if log:
         print_log(api_url)
