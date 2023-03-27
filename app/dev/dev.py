afd1dc8dabccf31fda42d99b3a4e170ea82ec99d
Mikko Heikkinen <hawk@biomi.org>
2023-03-28 00:00:56 +0300

    Demo of yearly animation, with max 10k records

--- app/dev/dev.py
+++ app/dev/dev.py
@@ -1,104 +1,56 @@
 
 from helpers import common_helpers
 
-def species_list(date_filter):
-    date_filter = date_filter.replace("/", "%2F")
+def get_species_dates(taxon, year):
+    
+    # TODO: Filter out osb with different start and end dates
+    limit = 10000
+    url = f"https://api.laji.fi/v0/warehouse/query/unit/list?selected=gathering.conversions.dayOfYearBegin%2Cgathering.conversions.dayOfYearEnd%2Cgathering.conversions.wgs84CenterPoint.lat%2Cgathering.conversions.wgs84CenterPoint.lon&orderBy=gathering.conversions.dayOfYearBegin&pageSize={ limit }&page=1&cache=true&taxonId={ taxon }&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&countryId=ML.206&time={ year }&collectionIdNot=HR.48&individualCountMin=1&qualityIssues=NO_ISSUES&access_token="
 
-    url = f"https://api.laji.fi/v0/warehouse/query/unit/aggregate?aggregateBy=unit.linkings.originalTaxon.speciesNameFinnish&onlyCount=true&taxonCounts=false&pairCounts=false&atlasCounts=false&excludeNulls=true&pessimisticDateRangeHandling=false&pageSize=1000&page=1&cache=true&taxonId=MX.37580&useIdentificationAnnotations=true&includeSubTaxa=true&includeNonValidTaxa=true&countryId=ML.206&yearMonth=2022%2F2025&individualCountMin=1&qualityIssues=NO_ISSUES&reliability=RELIABLE,UNDEFINED&time={ date_filter }&collectionIdNot=HR.48%2CHR.3671%2CHR.2029&access_token="
     data = common_helpers.fetch_finbif_api(url)
 #    print(data)
 
-    species_counts = dict()
-    count_sum = 0
+    observations_count = 0
+    observations_string = ""
 
-    for item in data["results"]:
-        species = item["aggregateBy"]["unit.linkings.originalTaxon.speciesNameFinnish"]
-        count = item["count"]
-        species_counts[species] = count
-        count_sum = count_sum + count
+    for obs in data["results"]:
+        # Skip if data is missing
+        if "dayOfYearBegin" not in obs["gathering"]["conversions"]:
+            continue
+        if "wgs84CenterPoint" not in obs["gathering"]["conversions"]:
+            continue
 
-    return species_counts, count_sum
+        # Skip long date ranges
+        if obs["gathering"]["conversions"]["dayOfYearEnd"] > (obs["gathering"]["conversions"]["dayOfYearBegin"] + 10):
+            continue
 
-
-def make_change_html(all_species):
-    
-    html = "<table class='styled-table'>\n"
-    html += "<thead>\n<tr>"
-    html += "<th>Laji</th>"
-    html += "<th>Vuotta aiemmin</th>"
-    html += "<th>Viimeiset 10 pv</th>"
-    html += "<th>Normalisoitu suhde</th>"
-    html += "<th>&nbsp;</th>"
-    html += "</tr>\n</thead>\n"
-    html += "<tbody>\n"
-
-    for species, data in all_species.items():
-        bar_width = int(data['proportion'] * 20)
-        css_class = ""
-
-        if 10 == data['proportion']:
-            data['proportion'] = "&gt;10"
-            css_class = "capped"
-        elif data['proportion'] >= 1:
-            css_class = "positive"
-        elif data['proportion'] < 1:
-            css_class = "negative"
-
-        html += "<tr>"
-        html += f"<td>{ species }</td>"
-        html += f"<td>{ data['count_last_year'] }</td>"
-        html += f"<td>{ data['count'] }</td>"
-        html += f"<td>{ data['proportion'] }</td>"
-        html += f"<td><span class='proportion_bar { css_class }' style='width: { bar_width }px;'>&nbsp;</span></td>"
-        html += "</tr>\n"
-        
-    html += "</thead>\n</table>\n"
-
-    return html
+        day = obs["gathering"]["conversions"]["dayOfYearBegin"]
+        lat = obs["gathering"]["conversions"]["wgs84CenterPoint"]["lat"]
+        lon = obs["gathering"]["conversions"]["wgs84CenterPoint"]["lon"]
+        observations_count += 1
+        observations_string += f"{{day: {day}, lat: {lat}, lng: {lon}}},"
 
 
-def make_change_dict(species_this_year, sum_this_year, species_last_year, sum_last_year):
-    proportion_cap = 10
-    all_species = dict()
-
-    for species, count in species_this_year.items():
-
-        # Species observed on both years
-        if species in species_last_year:
-            count_normalized = count / sum_this_year
-            count_last_year = species_last_year[species]
-            count_last_year_normalized = count_last_year / sum_last_year
-            proportion = round(count_normalized / count_last_year_normalized, 2)
-
-            if proportion > proportion_cap:
-                proportion = proportion_cap
-
-        # Species observed only this year
-        else:
-            proportion = proportion_cap
-            count_last_year = 0
-
-        single_species = dict()
-        single_species["count_last_year"] = count_last_year
-        single_species["count"] = count
-        single_species["proportion"] = proportion
-
-        all_species[species] = single_species
-
-    # Sort descending
-    sorted_all_species = dict(sorted(all_species.items(), key=lambda x: x[1]['proportion'], reverse=True))
-
-    return sorted_all_species
+    return observations_string, observations_count
 
 
-def main(param):
+def main(taxon_unsafe):
+
+    # MX.36237 peippo, 17k
+    # MX.36239 järri, 4800
+    # MX.28895 pöllöt
+    # MX.33638 Acrocephalus
+
+    # TODO: Clean taxon_unsafe
+    taxon = taxon_unsafe
+
     html = dict()
 
-    species_this_year, sum_this_year = species_list("-11/-1")
-    species_last_year, sum_last_year = species_list("-376/-366")
+    observations_string, observations_count = get_species_dates(taxon, 2022)
+    print(observations_count)
+#    print(observations_string)
 
-    all_species = make_change_dict(species_this_year, sum_this_year, species_last_year, sum_last_year)
-
-    html["data"] = make_change_html(all_species)
+    html["observations_string"] = observations_string
+    html["data"] = "Lintu"
 
     return html
