350362d64ca9bd96b8d9b525677b05cd893a43ba
Mikko Heikkinen <hawk@biomi.org>
2023-12-11 19:24:30 +0200

    Fix login module error handling

--- app/login/info.py
+++ app/login/info.py
@@ -1,13 +1,4 @@
 
-
-from helpers import common_helpers
-
-
 def main(user_data):
     html = dict()
-
-    print(user_data)
-    
-    html["test"] = "Foo bar"
-
     return html
--- app/login/login.py
+++ app/login/login.py
@@ -1,11 +1,4 @@
 
-
-from helpers import common_helpers
-
-
 def main(person_token_untrusted):
     html = dict()
-    
-    html["test"] = "Token: " + person_token_untrusted
-
     return html
