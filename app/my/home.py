d6f20a0edaef0fd46a85ff99f505f4a1b68052c4
Mikko Heikkinen <hawk@biomi.org>
2026-05-25 14:29:19 +0300

    Add links to group feature

--- app/my/home.py
+++ app/my/home.py
@@ -3,5 +3,7 @@ import datetime
 
 def main():
     current_year = datetime.datetime.now().year
-    years = [current_year, current_year - 1, current_year - 2]
-    return {"current_year": current_year, "years": years}
+    return {"current_year": current_year}
+
+
+
